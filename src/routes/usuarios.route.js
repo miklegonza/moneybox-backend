873bbb9bbdf94ea12cac0c078762933bef5f31d5
Michael González <66238404+miklegonza@users.noreply.github.com>
2022-10-07 15:14:11 -0500

    DB management v1

--- src/routes/usuarios.route.js
+++ src/routes/usuarios.route.js
@@ -1,2 +1,44 @@
 //Aquí van las rutas con express.Router(); (métodos HTTP)
 //Se llaman a los controladores
+
+const express = require('express');
+const router = express.Router();
+const usuarios = require('../services/usuarios.service');
+
+router.get('/', async (req, res, next) => {
+    try {
+        res.json(await usuarios.get(req.query.page));
+    } catch (err) {
+        console.error('Get error:', err.message);
+        next(err);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        res.json(await usuarios.create(req.body));
+    } catch (err) {
+        console.error('Post error:', err.message);
+        next(err);
+    }
+});
+
+router.put('/:username', async (req, res, next) => {
+    try {
+        res.json(await usuarios.update(req.params.username, req.body));
+    } catch (err) {
+        console.error('Post error:', err.message);
+        next(err);
+    }
+});
+
+router.delete('/:username', async (req, res, next) => {
+    try {
+        res.json(await usuarios.remove(req.params.username));
+    } catch (err) {
+        console.error('Post error:', err.message);
+        next(err);
+    }
+});
+
+module.exports = router;
--- src/services/usuarios.service.js
+++ src/services/usuarios.service.js
@@ -1,2 +1,38 @@
 //CRUD de la tabla usuarios
 // Aquí van las consultas SQL
+
+const db = require("./db.service");
+const helper = require("../utils/helper.util");
+const config = require("../configs/db.config");
+
+async function get() {
+    let stmt = "SELECT FROM";
+    const rows = await db.query("Select statement");
+    const data = helper.emptyOrRows(rows);
+    const meta = { page };
+
+    return { data, meta };
+}
+
+async function create(user) {
+    const user = {
+        name,
+    };
+    let stmt = "INSERT INTO";
+    const result = await db.query(" statement");
+    return result.affectedRows ? "Usuario creado" : "Error al crear usuario";
+}
+
+async function update(username, user) {
+    let stmt = "UPDATE SET";
+    const result = await db.query(" statement");
+    return result.affectedRows ? "Usuario creado" : "Error al crear usuario";
+}
+
+async function remove(username) {
+    let stmt = "DELETE FROM";
+    const result = await db.query(" statement");
+    return result.affectedRows ? "Usuario creado" : "Error al crear usuario";
+}
+
+module.exports = { get, create, update, remove };
